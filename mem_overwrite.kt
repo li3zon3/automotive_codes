db4acec9cf82ab97ace4c6a37f7c4c84f1ac2463
Li3zon3 <li3zon3@DESKTOP-S8Q8U5B>
2023-03-28 19:57:13 +0700

    none

--- mem_overwrite.kt
+++ mem_overwrite.kt
@@ -1,3 +1,5 @@
+package com.example.mem_overwrite
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -21,14 +23,18 @@ class MainActivity : AppCompatActivity() {
             try {
                 val process = Runtime.getRuntime().exec(command)
                 val reader = BufferedReader(InputStreamReader(process.inputStream))
+                val errorReader = BufferedReader(InputStreamReader(process.errorStream))
                 val output = StringBuilder()
                 var line: String?
                 while (reader.readLine().also { line = it } != null) {
                     output.append(line).append("\n")
                 }
+                while (errorReader.readLine().also { line = it } != null) {
+                    output.append(line).append("\n")
+                }
                 resultText.text = output.toString()
             } catch (e: Exception) {
-                resultText.text = "Error executing command: ${e.message}"
+                resultText.text = "Error executing command: ${e.message}\n\n${e.stackTraceToString()}"
             }
         }
     }
